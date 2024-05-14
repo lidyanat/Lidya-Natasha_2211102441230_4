9f822de53b95b6ceac2c7222ee87fc5df6ec5bca
lidyanat <162877600+lidyanat@users.noreply.github.com>
2024-05-14 14:39:42 +0800

    Add files via upload

--- flutter_export_environment.sh
+++ flutter_export_environment.sh
@@ -3,7 +3,6 @@
 export "FLUTTER_ROOT=D:\flutter_vscode_version\flutter_application_1\flutter"
 export "FLUTTER_APPLICATION_PATH=D:\navigasi_widget_hero"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
